fb948105dffd2f57844b3e89b740baa3d16417f5
YongTae JIN <freview@nate.com>
2024-09-11 17:13:39 +0900

    f230911

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
 	   <v-app-bar app color="primary" dark hide-on-scroll>
 		   <v-app-bar-nav-icon @click="toggleDrawer" />
 		   <!-- <site-title /> 테라스트림 스마트공방 생산관리 시스템 -->
-		   <site-title /> (주)테라스트림 생산관리시스템
+		   <!-- <site-title /> (주)테라스트림 생산관리시스템 -->
+		   <site-title /> Mi MES
 		   <v-spacer></v-spacer> 
 		   <template v-if="this.$store.state.user.member">{{this.$store.state.user.member.n_comtoname}}</template>
 		   
